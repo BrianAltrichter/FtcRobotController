e8db261e9d344425ec8864a2fdbba79c368103fb
BrianAltrichter <bcaltrichter@gmail.com>
2022-11-05 10:46:12 -0400

    Update ColorDetection.java
    Worked on connecting the robot to the control hub.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/ColorDetection.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import org.firstinspires.ftc.teamcode.wrappers.JoystickWrapper;
+
+@TeleOp
+public class ColorDetection extends LinearOpMode {
+    DcMotor Motor1;
+    JoystickWrapper joystickWrapper;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Motor1 = hardwareMap.get(DcMotor.class,"bottomMotor");
+        joystickWrapper = new JoystickWrapper(gamepad1, gamepad2);
+        waitForStart();
+
+
+        while (!isStopRequested()) {
+            System.out.println(joystickWrapper.gamepad1GetLeftStickX());
+            Motor1.setPower(joystickWrapper.gamepad1GetLeftStickX());
+        }
+        Motor1.setPower(0);
+    }
+}
